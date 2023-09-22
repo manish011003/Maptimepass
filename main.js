cd0e13d3302bc547e04db13746628043f0d739a4
manish011003 <m4n15hb@gmial.com>
2023-09-23 00:07:49 +0530

    First commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const apikey = 'pk.eyJ1Ijoic2FtLXRoZS1tYW4iLCJhIjoiY2xjOTVqZ29rMHlubDNxcDJ0MG9rZTloMiJ9.xQ4pFUSlPnxPV7VJqhJqbA';
-const map = L.map('map').setView([23.843141, 91.423968], 17);
+const map = L.map('map').setView([23.840831, 91.421521], 17);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    maxZoom: 19,
+    maxZoom: 50,
     id: 'mapbox/satellite-v9',
     accessToken: apikey,
     zoomOffset: -1,
